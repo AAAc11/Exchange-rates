530f5f68aed1f5ed18cee58f3190f45885dc395a
Anna <annacygan344@gmail.com>
2026-09-23 13:42:35 +0200

    Add API handling and raw_data folder

---
+++ load_data.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from datetime import datetime
+import os
+
+def get_raw_data():
+    URL = "https://api.nbp.pl/api/exchangerates/tables/a/today/?format=json"
+
+    try:
+        raw_data = requests.get(URL)
+        raw_data.raise_for_status()
+        raw_data_json = raw_data.json()
+
+        os.makedirs("raw_data", exist_ok=True)
+
+        today_date = datetime.now().date()
+        with open(f"raw_data/{today_date}.json", "w", encoding="utf-8") as file:
+            json.dump(raw_data_json, file, indent=4)
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
